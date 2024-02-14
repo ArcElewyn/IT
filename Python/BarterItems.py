25b19786c4305ae407809f49c5577aa9df53082b
ArcElewyn <ph.elewyn@gmail.com>
2024-02-14 14:24:46 +0100

    initial commit

---
+++ Python/BarterItems.py
@@ -0,0 +1,37 @@
+# 
+
+choix = 0
+while not (choix =='quit') :
+    
+    #  Menu
+    print("""hello, here is a quick script to count the barter items required for your hideout
+    (source: https://escapefromtarkov.fandom.com/)
+    - If you want to edit your hideout type 1
+    - If you want to see the quantity of an item required type 2
+    - If you want to add an item to your stash, type 3
+    - If you want to remove an item from your stash, type 4
+    - If you want to leave, type quit""")
+
+    # Ask 
+    choix = input("what are we doing today? ")
+
+    match choix:
+        case '1' :
+            print("edit hideout upgrades")
+            input ("press a a key to continue")
+        case '2' :
+            itemtolist = input ("name of the item you want to list? ")
+            print("you still need x", itemtolist)
+            input ("press a a key to continue")
+        case '3' :
+            numberofitem = input ("how many of said item do you want to add? ")
+            itemtoadd = input ("name of the desired item? ")
+            print("added", numberofitem , itemtoadd)
+            input ("press a a key to continue")
+        case '4' :
+            numberofitem = input ("how many of said item do you want to remove? ")
+            itemtoremove = input ("name of the desired item? ")
+            print("removed", numberofitem , itemtoremove)
+            input ("press a a key to continue")
+        case 'quit' :
+            exit
---
+++ Python/learnings.md
@@ -0,0 +1,31 @@
+# Python with VsCode
+
+### 1- Pre-requisite
++ Git is installed on the system
++ Vs Code is installed on the system
++ A python interpreter installed on the system
++ A virtual environnement is created on VsCode \
+&nbsp;&nbsp;&nbsp; First, type Ctrl + shift + P to go in command mode in VsCode \
+ &nbsp;&nbsp;&nbsp; type *Python: Create Environement* ,select the venv option, set the path of the environement, \
+ &nbsp;&nbsp;&nbsp; then select the option" + enter interpreter path" 
++ Import your libraries \
+&nbsp;&nbsp;&nbsp; Go in Commande mode for vscode and type Python: create terminal \
+&nbsp;&nbsp;&nbsp; In the terminal, type *pip install "name of the library"*
+
+### 2- Commands
+
++ *Print ()* :  print the message or the value of the variable, to print multiple lines, add 3 quote before and after the message you want to print.
++ *Input()* : wait for the user to input something, can be used to set variable.
++ *match case* : set a variable then match will check the content of said variable, and the script will continue based on the content 
+> match examplevariable :\
+&nbsp;&nbsp;&nbsp;case 'number' : \
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;print("number of files")\
+&nbsp;&nbsp;&nbsp;case 'size' : \
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;print("size of the folder")\
+&nbsp;&nbsp;&nbsp;case 'delete' : \
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;print("delete file")\
+
+
+
+
+
