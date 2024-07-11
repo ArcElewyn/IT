1dfde749c466f35030273621574e59cb2ff4ec62
Mickaël <159157671+ArcElewyn@users.noreply.github.com>
2024-07-11 15:53:05 +0200

    Update huntshowloadout.py
    Finished the main and secondary weapon randomizer, and fixed a few typos

--- Python/huntshowloadout.py
+++ Python/huntshowloadout.py
@@ -24,18 +24,18 @@ clear()
 
 # Roll main weapon if needed
 if main == 'yes':
-    main = random.choice([])
-    print("your main weapon is:",main)
+    main = random.choice(['Romero 77','Caldwell Rival 78','Springfield 1866','Winfield M1873','Winfield M1873C','Winfield M1876 Centennial','Vetterli 71 Karabiner','Martini-Henry IC1','Caldwell Marathon','Mako 1895 Carbine','Sparks LRR','Winfield 1887 Terminus','Winfield 1893 Slate','Drilling','Specter 1882','Berthier Mle 1892','Lebel 1886','Mosin-Nagant M1891','Crown & King Auto-5','Nitro Express Rifle','Railroad Hammer','Bomb Lance','Combat Axe','Katana','Crossbow','Bow'])
+    print("your main weapon is: a",main)
 
 # Roll secondary weapon if needed
 if secondary == 'yes':
-    secondary = random.choice([])
-    print("your secondary weapon is:",secondary)
+    secondary = random.choice(['Nagant M1895','Caldwell Conversion Pistol','Caldwell 92 New Army','Caldwell Pax','LeMat Mark II Revolver','Bornheim No. 3','Scottfield Model 3','Dolch 96','Cavalry Saber','Baseball Bat','Machete','Hand Crossbow'])
+    print("your secondary weapon is: a",secondary)
 
 # Roll melee weapon if needed
 if melee == 'yes':
     melee = random.choice(['Knife','Heavy knife','Dusters','Knuckle Knife','Throwing Axes','Throwing Knives','Throwing Spear'])
-    print("your melee weapon is:",melee)
+    print("your melee weapon is: a",melee)
 
 # Roll set number of consumables
 consumableslist = ['Ammo box', 'Medical Pack', 'Tool Box', 'Antidote Shot', 'Weak Antidote Shot', 'Regeneration Shot', 'Weak Regeneration Shot', 'Stamina Shot', 'Weak Stamina Shot', 'Vitality Shot', 'Weak Vitality Shot', 'Firebomb', 'Liquid Fire Bomb', 'Hellfire Bomb', 'Choke Beetle', 'Stalker Beetle', 'Fire Beetle', 'Chaos Bomb', 'Sticky Bomb', 'Poison Bomb', 'Hive Bomb', 'Flash Bomb', 'Concertina Bomb', 'Frag Bomb', 'Big Dynamite Bundle', 'Dynamite Bundle', 'Dynamite Stick', 'Waxed Dynamite Stick']
@@ -43,30 +43,30 @@ consumableslist = ['Ammo box', 'Medical Pack', 'Tool Box', 'Antidote Shot', 'Wea
 match consumables:
     case '1':
         first = random.choice(consumableslist)
-        print("first consumable is:",first)
+        print("first consumable is: a",first)
 
     case '2':
         first = random.choice(consumableslist)
-        print("first consumable is:",first)
+        print("first consumable is: a",first)
         second = random.choice(consumableslist)
-        print("second consumable is:",second)
+        print("second consumable is: a",second)
 
     case '3':
         first = random.choice(consumableslist)
-        print("first consumable is:",first)
+        print("first consumable is: a",first)
         second = random.choice(consumableslist)
-        print("second consumable is:",second)
+        print("second consumable is: a",second)
         third = random.choice(consumableslist)
-        print("third consumable is:",third)
+        print("third consumable is: a",third)
 
     case '4':
         first = random.choice(consumableslist)
-        print("first consumable is:",first)
+        print("first consumable is: a",first)
         second = random.choice(consumableslist)
-        print("second consumable is:",second)
+        print("second consumable is: a",second)
         third = random.choice(consumableslist)
-        print("third consumable is:",third)
+        print("third consumable is: a",third)
         fourth = random.choice(consumableslist)
-        print("Fourth consumable is:",fourth)
+        print("Fourth consumable is: a",fourth)
    
 input("Job's done")
