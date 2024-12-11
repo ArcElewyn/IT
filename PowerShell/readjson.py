89d4a75dfb7cf72ef61947742f59da32060eeb85
Mickaël <159157671+ArcElewyn@users.noreply.github.com>
2024-12-11 14:27:05 +0100

    Add files via upload
    added a .py that I wrote and is used to parse a json file and extract data from it

---
+++ PowerShell/readjson.py
@@ -0,0 +1,39 @@
+# Import module
+import json
+import os
+import csv
+import sys
+
+
+jsonPath = sys.argv[1]
+
+# Clear the content of hosts_paths.csv
+with open("/var/jenkins_home/jobs/SID_EtudeParcAppli/workspace/jenkins/EtudeParcAppli/hosts_paths.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(["App", "Env", "Host", "Path"])
+
+# Open json file
+with open(jsonPath, 'r') as f:
+    data = json.load(f)
+
+# For each app, list app name, env, hostname, and the path of the app on the host and write it on .csv
+for app_name, app_data in data.items():
+    for env_data in app_data:
+        try:
+            for env_name, env_info in env_data.items():
+                hosts = env_info.get("host", [])
+                if isinstance(hosts, list):
+                    for host in hosts:
+                        host_name = host.get("host", "Unknown Host")
+                ssh_app_dir = env_info.get("sshAppDir", "Unknown sshAppDir")
+                deploy_dir = env_info.get("deployDir", "Unknown deployDir")
+                with open("/var/jenkins_home/jobs/SID_EtudeParcAppli/workspace/jenkins/EtudeParcAppli/hosts_paths.csv", "a", newline='') as f:
+                    writer = csv.writer(f)
+                    if deploy_dir.startswith("/"):
+                        writer.writerow([app_name, env_name, host_name, f"{ssh_app_dir.rstrip('/')}{deploy_dir}"])
+                    elif deploy_dir == "":
+                        writer.writerow([app_name, env_name, host_name, ssh_app_dir])
+                    elif not deploy_dir.startswith("/"):
+                        writer.writerow([app_name, env_name, host_name, f"{ssh_app_dir.rstrip('/')}/{deploy_dir}"])               
+        except Exception as app_error:
+            print("Error processing application " + app_name + " : " + str(app_error))
