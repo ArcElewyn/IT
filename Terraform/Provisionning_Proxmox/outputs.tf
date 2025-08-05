b87e146442e6dd709965d8ffa806e766e9ef3bd7
Mickaël <159157671+ArcElewyn@users.noreply.github.com>
2025-08-05 09:20:26 +0200

    Delete Terraform/Provisionning_Proxmox/outputs.tf

--- Terraform/Provisionning_Proxmox/outputs.tf
+++
@@ -1,9 +0,0 @@
-
-output "vm_name" {
-  description = "Name of the created VM"
-  value       = proxmox_virtual_environment_vm.debian_vm.name
-}
-
-output "vm_ip" {
-  value = proxmox_virtual_environment_vm.debian_vm.default_ipv4_address
-}
